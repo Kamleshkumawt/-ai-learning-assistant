759449693e512cd5c87412d7f6513c0d80ca6c31
kamleshkumwt12th@gmail.com <kamleshkumwt12th@gmail.com>
2026-02-09 22:57:02 +0530

    feat(models): create schemas for flashcard, document, and chatHistory

---
+++ Backend/models/document.model.js
@@ -0,0 +1,68 @@
+import mongoose from "mongoose";
+
+const documentSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "user",
+      required: true,
+    },
+    title: {
+      type: String,
+      trim: true,
+      required: [true, "Please provide a document title"],
+    },
+    fileName: {
+      type: String,
+      required: true,
+    },
+    filePath: {
+      type: String,
+      required: true,
+    },
+    fileSize: {
+      type: Number,
+      required: true,
+    },
+    extractedText: {
+      type: String,
+      default: "",
+    },
+    chunks: [
+      {
+        content: {
+          type: String,
+          required: true,
+        },
+        pageNumber: {
+          type: Number,
+          default: 0,
+        },
+        chunkIndex: {
+          type: Number,
+          required: true,
+        },
+      },
+    ],
+    uploadDate: {
+      type: Date,
+      default: Date.now,
+    },
+    lastAccessed: {
+      type: Date,
+      default: Date.now,
+    },
+    status: {
+      type: String,
+      enum: ["processing", "ready", "failed"],
+      default: "processing",
+    },
+  },
+  { timestamps: true },
+);
+
+//Index for faster queries
+documentSchema.index({ userId: 1, uploadDate: -1 });
+
+const Document = mongoose.model("document", documentSchema);
+export default Document;
---
+++ Backend/models/flashcard.model.js
@@ -0,0 +1,49 @@
+import mongoose from "mongoose";
+
+const flashcardSchema = new mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "user",
+        required: true
+    },
+    documentId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "document",
+        required: true
+    },
+   cards: [{
+    question: {
+        type: String,
+        required: true
+    },
+    answer: {
+        type: String,
+        required: true
+    },
+    difficulty: {
+        type: String,
+        enum: ['easy', 'medium', 'hard'],
+        default: 'medium',
+    },
+    lastReviewed: {
+        type: Date,
+        default: null
+    },
+    reviewCount: {
+        type: Number,
+        default: 0
+    },
+    isStarred: {
+        type: Boolean,
+        default: false
+    },
+   }],
+},{
+    timestamps: true,
+});
+
+// index for faster queries
+flashcardSchema.index({ userId: 1, documentId: 1 });
+
+const Flashcard = mongoose.model("flashcard", flashcardSchema);
+export default Flashcard;
